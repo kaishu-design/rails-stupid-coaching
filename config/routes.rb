6409eb96295b1132c4038bea51b2385f533feceb
exec zsh <kaishu0520@gmail.com>
2023-08-07 13:56:45 +0900

    didnt do an optional

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # verb "url", to: "controller#action"
+  get 'ask', to: 'questions#ask', as: 'ask'
+  get 'answer', to: 'questions#answer', as: 'answer'
   # Defines the root path route ("/")
   # root "articles#index"
 end
