963ca62e238eef42c33f438f45d0f46e98e34696
exec zsh <kaishu0520@gmail.com>
2024-11-24 16:26:56 +0800

    edit of ask view and funcitoned the user is able to select option

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,12 +5,12 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:query]
-    if @question == 'I am going to work'
-      @answer = 'Great!'
+    if @question == 'I am going to work(仕事に行ってきます)'
+      @answer = 'Great! 頑張って行ってらっしゃい👋'
     elsif @question.include?('?')
-      @answer = 'Silly question, get dressed and go to work!'
+      @answer = 'Silly question, get dressed and go to work!  早く準備して仕事に行きなさい'
     else
-      @answer = "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work! 早く準備して仕事に行きなさい"
     end
   end
 end
